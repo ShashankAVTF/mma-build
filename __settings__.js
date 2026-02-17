39873a4427f6e5a7738e7ff77c499e76aed1e8b7
ShashankAVTF <87561494+Cyanide1013@users.noreply.github.com>
2026-02-17 17:54:13 +0530

    build1

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2394425.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 269397551, 269397603, 269397673, 269397553, 269397606, 269397675, 269397569, 269397680, 269397549, 269397561, 269397539, 269397616, 269397590, 269397676, 269397653, 269397621, 269397622, 269397658, 269397694, 269397555, 269397668, 269397557, 269397584, 269397617, 269397566, 269397534, 269397702, 269397701, 276346889, 276366946 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/269397586/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/269397601/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/269397663/1/ammo.js', 'preload' : true},
+];
---
+++ files/assets/269397659/1/README.txt
@@ -0,0 +1,4 @@
+To use this project, you need to get Photon Realtime SDK and AppID from the official website(https://www.photonengine.com/).
+
+For more information, please see this tutorial.
+https://developer.playcanvas.com/en/tutorials/real-time-multiplayer-photon/
