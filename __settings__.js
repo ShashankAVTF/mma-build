b88a61a6e0344f79807316d5a72d2220cceecf5a
ShashankAVTF <87561494+Cyanide1013@users.noreply.github.com>
2026-02-18 12:34:01 +0530

    build 3

--- __settings__.js
+++ __settings__.js
@@ -6,7 +6,7 @@ window.CONTEXT_OPTIONS = {
     'alpha': false,
     'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
-    'powerPreference': "low-power"
+    'powerPreference': "default"
 };
 window.SCRIPTS = [ 269397551, 269397603, 269397673, 269397553, 269397606, 269397675, 269397569, 269397680, 269397549, 269397561, 269397539, 269397616, 269397590, 269397676, 269397653, 269397621, 269397622, 269397658, 269397694, 269397555, 269397668, 269397557, 269397584, 269397617, 269397566, 269397534, 269397702, 269397701, 276346889 ];
 window.CONFIG_FILENAME = "config.json";
@@ -19,4 +19,5 @@ window.INPUT_SETTINGS = {
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
     {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/269397586/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/269397601/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/269397663/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/276487821/1/basis.wasm.js', 'wasmUrl' : 'files/assets/276487819/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/276487820/1/basis.js', 'preload' : false},
 ];
